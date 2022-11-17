3ef2d3c6b5be1249ec4c0fd1ad8abebe7f124d5d
Dean Lofts <dean@deanlofts.xyz>
2022-11-17 21:46:00 +1100

    formatting

--- app.rb
+++ app.rb
@@ -7,31 +7,31 @@ how_many = 3
 template_path = ".github/ISSUE_TEMPLATE/1-on-1-directreport-template.md"
 
 def get_random_question(questions)
-    random_index = rand(questions.length)
-    questions[random_index]
-    end
+  random_index = rand(questions.length)
+  questions[random_index]
+end
 
 def get_random_question_by_category(questions, category)
-    random_question = questions.select { |question| question["category"] == category }
-    if random_question.length == 0
-        return get_random_question(questions)
-    end
-    random_index = rand(random_question.length)
-    random_question[random_index]
+  random_question = questions.select { |question| question["category"] == category }
+  if random_question.length == 0
+    return get_random_question(questions)
+  end
+  random_index = rand(random_question.length)
+  random_question[random_index]
 end
 
 payload = categories.map do |category|
-    these_questions = []
-    how_many.times do
-        these_questions << get_random_question_by_category(questions, category)
-    end
-    { category => these_questions }
+  these_questions = []
+  how_many.times do
+    these_questions << get_random_question_by_category(questions, category)
+  end
+  { category => these_questions }
 end
 
 template = File.read(template_path)
 template.gsub!(/<!-- start of questions -->.*<!-- end of questions -->/m, "<!-- start of questions -->\n#{payload.map do |category|
-    "## #{category.keys[0]}\n\n#{category.values[0].map { |question| "- #{question["question"]}" }.join("\n\n")}\n"
+  "## #{category.keys[0]}\n\n#{category.values[0].map { |question| "- #{question["question"]}" }.join("\n\n")}\n"
 end
-.join("\n")}\n<!-- end of questions -->")
+  .join("\n")}\n<!-- end of questions -->")
 
 File.write(template_path, template)
