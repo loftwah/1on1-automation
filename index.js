d3e5c2317a15ef79fc1f920eb0c4416e6fee0e99
Dean Lofts <dean@deanlofts.xyz>
2022-11-17 18:47:03 +1100

    improve the questions

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-// read questions.json file and set it to a constant
 const questions = require('./questions.json');
+const categories = [...new Set(questions.map(question => question.category))];
 
-// create a function that will return a random question
+// function that will return a random question
 function getRandomQuestion() {
     const randomIndex = Math.floor(Math.random() * questions.length);
     return questions[randomIndex];
     }
 
-// read all of the category values from the questions.json file and set it to a constant
-const categories = questions.map(question => question.category);
-
-// print all of the categories to the console
-console.log(categories);
-
-// create a function that will return a random question from the conversation starters category
-function getRandomConversationStarter(category) {
-    const conversationStarters = questions.filter(question => question.category === category);
-    const randomIndex = Math.floor(Math.random() * conversationStarters.length);
-    return conversationStarters[randomIndex];
-    }
-
-// create the same as above but if the category isn't present, return a random question
+// as above but if the category isn't present, return a random question
 function getRandomQuestionByCategory(category) {
-    const conversationStarters = questions.filter(question => question.category === category);
-    if (conversationStarters.length === 0) {
+    const randomQuestion = questions.filter(question => question.category === category);
+    if (randomQuestion.length === 0) {
         return getRandomQuestion();
     }
-    const randomIndex = Math.floor(Math.random() * conversationStarters.length);
-    return conversationStarters[randomIndex];
+    const randomIndex = Math.floor(Math.random() * randomQuestion.length);
+    return randomQuestion[randomIndex];
     }
 
-// print two random questions from each category to the console
+// ## Category
+// 
+// - question
+//
+// - question
+//
+// print two random questions from each category to the console following the above format
 categories.forEach(category => {
-    console.log(getRandomQuestionByCategory(category));
-    console.log(getRandomQuestionByCategory(category));
+    console.log(`## ${category}`);
+    console.log('');
+    console.log(getRandomQuestionByCategory(category).question);
+    console.log('');
+    console.log(getRandomQuestionByCategory(category).question);
+    console.log('');
     }
 );
